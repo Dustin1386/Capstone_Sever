0613d26107aacb35f0a34c52b04e79ef84183850
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2020-11-20 15:28:01 -0700

    added direction

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://movie-kohl.vercel.app/'
+}))
 app.use(express.json())
 
 //get all films
