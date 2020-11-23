e9aa4a098ee25d8e1f88475ee73f6eb90c02e5ba
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2020-11-23 13:31:27 -0700

    stuff

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,7 @@ console.log(process.env.DATABASE_URL)
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.NODE_ENV ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
+  "connectionString": process.env.NODE_ENV === "test" ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
 
      
 }
