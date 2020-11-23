bded74bacffd40e7f61d60f8c630012cd785d3fe
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2020-11-23 10:16:09 -0700

    added postgrator stuff

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-
+console.log(process.env.DATABASE_URL)
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DB_URL ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
+  "connectionString": process.env.NODE_ENV ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
 
      
 }
